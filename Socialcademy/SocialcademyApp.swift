4150d242ce67a4f92fffface3e91ee604c94b6d2
Peter <Petermoeinlewis@gmail.com>
2022-06-22 03:31:12 -0700

    Initial Commit

---
+++ Socialcademy/SocialcademyApp.swift
@@ -0,0 +1,17 @@
+
+
+import SwiftUI
+import Firebase
+
+@main
+struct SocialcademyApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            AuthView()
+        }
+    }
+}
